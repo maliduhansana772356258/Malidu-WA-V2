c16406889168885a62f99ab0c24dbeb0cc6cfc23
maliduhansana772356258 <130507902+maliduhansana772356258@users.noreply.github.com>
2025-01-15 01:14:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'add your sesson id',
+    SESSION_ID: process.env.SESSION_ID || 'BGdjGB5J#pt9me80JQXPZ_puFg8g8VroxzhffljS2NUb2sEjsQIY',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     ALIVE_IMG: process.env.ALIVE_IMG || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2DsJTn1Zq-Ot2HfC0AJyyxO813o6pXewVCw&s",
     ALIVE_MSG: process.env.ALIVE_MSG || "Hii...I am Alive Now ..",
