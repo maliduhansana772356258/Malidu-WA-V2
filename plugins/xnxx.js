e8ba5ad867af4fb8c2e24014cb78d797255ecec5
maliduhansana772356258 <130507902+maliduhansana772356258@users.noreply.github.com>
2025-01-15 11:43:42 +0530

    Update xnxx.js

--- plugins/xnxx.js
+++ plugins/xnxx.js
@@ -1,1 +1,19 @@
+const { cmd, commands } = require('../command');
+const { fetchJson } = require('../lib/functions');
+const domain = `https://manu-ofc-api-site-6bfcbe0e18f6.herokuapp.com`;
+const api_key = `Manul-Official-Key-999`;
 
+//============================================
+
+cmd({
+    pattern: "nsfw",
+    alias: ["xnxxdl", "xnxxdownload"],
+    react: '🫣',
+    category: "download",
+    desc: "Search videos on xnxx and get download links",
+    filename: __filename
+}, async (conn, m, mek, { from, isMe, isOwner, q, reply }) => {
+    try {
+        // Check if search query is provided
+        if (!q || q.trim() === '') return await reply('*Please provide a search query! (e.g., Deadpool)*');
+        if (!isMe && !isOwner) return await reply('*Only Bot Number Can Movie Download !!!*');
